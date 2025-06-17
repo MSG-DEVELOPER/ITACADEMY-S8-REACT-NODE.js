80e2004e1715183b605881d9bc6a87a350f2602e
Joker <puebloseko@gmail.com>
2025-06-17 12:28:54 +0200

    WIP working server

--- src/components/Shared/NavbarShared/NavbarShared.tsx
+++ src/components/Shared/NavbarShared/NavbarShared.tsx
@@ -1,8 +1,29 @@
 
+import { NavbarContainer, NavItem, NavList, Logo } from "./NavbarShared.style";
+import { Link, useLocation } from "react-router-dom";
+
 function NavbarShared() {
+  const location = useLocation();
+
   return (
-    <div>NavbarShared</div>
-  )
+    <NavbarContainer>
+      <Logo>S8-ITAcademy</Logo>
+      <NavList>
+        <NavItem isActive={location.pathname === "/"}>
+          <Link to="/">Inicio</Link>
+        </NavItem>
+        <NavItem isActive={location.pathname === "/map"}>
+          <Link to="/map">Mapa</Link>
+        </NavItem>
+        <NavItem isActive={location.pathname === "/grafic"}>
+          <Link to="/grafic">Gráficos</Link>
+        </NavItem>
+        <NavItem isActive={location.pathname === "/calendar"}>
+          <Link to="/calendar">Calendario</Link>
+        </NavItem>
+      </NavList>
+    </NavbarContainer>
+  );
 }
 
-export default NavbarShared
+export default NavbarShared;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 
 function HomePage() {
   return (
-    <div>Home</div>
+    <div></div>
   )
 }
 
